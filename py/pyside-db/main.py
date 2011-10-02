b047c73f7c236202062b3f1b20834c031ee08dd5
lishnih <lishnih@40f2ffb2-f6ac-11de-a7af-57dd9f136cd4>
2011-10-02 15:48:44 +0000

    Перенёс лог в отдельный модуль.

--- py/pyside-db/main.py
+++ py/pyside-db/main.py
@@ -2,28 +2,11 @@
 # -*- coding: utf-8 -*-
 # Stan 2011-06-22
 
-import sys, os
-import logging.config
-import yaml
+import sys, logging
 from PySide import QtCore, QtGui    # GUI
 
+import init_logging                 # Логи
 import mainframe                    # Основное окно
-
-
-def init_logging():
-    global text_log_filename, html_log_filename
-    current_dir = os.path.dirname(__file__)
-    log_dir = os.path.join(current_dir, "logs")
-    # log_dir = os.path.join(os.path.expanduser("~"), "pydata")
-    if not os.path.isdir(log_dir):
-        os.makedirs(log_dir)
-    text_log_filename = os.path.join(log_dir, "log.txt")
-    html_log_filename = os.path.join(log_dir, "log.html")
-
-    log_conf_filename = os.path.join("conf", "log.yaml")
-    stream = file(log_conf_filename)
-    config = yaml.load(stream)
-    logging.config.dictConfig(config)
 
 
 def init_translator():
@@ -35,7 +18,6 @@ def init_translator():
 def main():
 #   init_translator()                       # Настройка i18n
 
-    init_logging()                          # Настройка логов
     logging.debug("Start!")
 
     app = QtGui.QApplication(sys.argv)      # Приложение
